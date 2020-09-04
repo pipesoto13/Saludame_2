7db17a2b9c6c0ad0dc77fd803ef9a6d6219d511b
Felipe Soto C <pipesoto13@gmail.com>
2020-09-03 22:46:11 -0500

    -add h1 tag

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/makers/:name', (req, res) => {
     var name = req.params.name;
     var name = name.charAt(0).toUpperCase() + name.slice(1);
-    res.send('Hola ' + name + '!');
+    res.send('<h1>Hola ' + name + '!</h1>');
   })
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
